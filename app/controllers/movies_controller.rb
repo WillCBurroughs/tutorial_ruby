2c4e724c1243a0e7d42f7f2f4a6c3549c38f35f9
William Burroughs <willcburroughs@gmail.com>
2024-03-15 08:28:58 -0400

    adding reviews first version

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,53 @@
 class MoviesController < ApplicationController
     def index
         # Correct way to dynamically show values
-        @movies = ["Iron man", "Superman", "Spider-man", "Batman"]
+        @movies = Movie.released
     end 
+
+    def show 
+        @movie = Movie.find(params[:id])
+    end
+
+    def edit    
+        @movie = Movie.find(params[:id])
+    end
+
+    def update
+        @movie = Movie.find(params[:id])
+        if @movie.update(movie_params)
+            redirect_to @movie, notice: "Movie successfully updated!"
+          else
+            render :edit, status: :unprocessable_entity
+          end
+    end
+
+    def new
+        @movie = Movie.new
+    end
+
+    def create
+        @movie = Movie.new(movie_params)
+        
+        if @movie.save
+            redirect_to @movie, notice: "Movie successfully created!"
+        else
+            render :new, status: :unprocessable_entity
+        end
+
+    end
+
+    def destroy
+        @movie = Movie.find(params[:id])
+        @movie.destroy
+        redirect_to movies_url, status: :see_other,
+        alert: "Movie successfully deleted!"
+    end
+
+    private
+
+    def movie_params
+      params.require(:movie).
+        permit(:title, :description, :rating, :released_on, :total_gross, :director, :duration, :image_file_name)
+    end
+
 end
